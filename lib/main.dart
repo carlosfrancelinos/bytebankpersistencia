db3d353e4bc4f83b6c938de3d430a440fc3daddf
carlosfrancelinos <francelinos@gmail.com>
2022-01-27 06:50:21 -0300

    adicionando arquivos

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:bytebanknew/screens/dashboard.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(BytebankApp());
+}
+
+class BytebankApp extends StatelessWidget {
+  const BytebankApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.green[900],
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.teal,
+        ).copyWith(secondary: Colors.blueAccent[700]),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+          primary: Colors.blueAccent[700],
+        )),
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.blueAccent[700],
+          textTheme: ButtonTextTheme.primary,
+        ),
+      ),
+      home: Dashboard(),
+    );
+  }
+}
