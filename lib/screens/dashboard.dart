03c2c94606a99bbe56d2badfa9af986c09b10722
carlosfrancelinos <francelinos@gmail.com>
2022-01-27 06:47:32 -0300

    First commit

---
+++ lib/screens/dashboard.dart
@@ -0,0 +1,59 @@
+import 'package:bytebanknew/screens/contacts_lista.dart';
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dashboard'),
+        backgroundColor: Colors.green[900],
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset('images/bytebank_logo.png'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Material(
+              color: Theme
+                  .of(context)
+                  .primaryColor,
+              child: InkWell(
+                onTap: () {
+                  Navigator.of(context).push(
+                      (MaterialPageRoute(
+                        builder: (context) => ContactsList(),
+                      ))
+                  );
+                },
+                child: Container(
+                    height: 100,
+                    width: 150,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Icon(
+                          Icons.people,
+                          color: Colors.white,
+                          size: 24.0,
+                        ),
+                        Text(
+                          'Contacts',
+                          style: TextStyle(color: Colors.white, fontSize: 16.0),
+                        )
+                      ],
+                    )),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
